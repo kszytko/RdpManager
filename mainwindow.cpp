f7cc765dcc295c156a1eedf7d080ed19257e2221
KS <mail@kszytko.pl>
2022-02-23 17:45:57 +0100

    added model set

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "treemodel.h"
+
+#include <QFile>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    const QStringList headers({tr("Title"), tr("Description")});
+
+    QFile file(":/default.txt");
+    file.open(QIODevice::ReadOnly);
+    TreeModel *model = new TreeModel(headers, file.readAll(), this);
+    file.close();
+
+    ui->mainTreeView->setModel(model);
+
+
+
 }
 
 MainWindow::~MainWindow()
